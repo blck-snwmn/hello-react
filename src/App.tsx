b2315cbddc79c6823e0ae0dac084e47785e2698a
blck-snwmn <whitesnowmancreator@gmail.com>
2019-07-24 12:37:38 +0900

    update reflect input to other input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+  const [str, setStrState] = useState("a");
+  const inputValue = useRef<HTMLInputElement>(null);
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div>
+        <label htmlFor="input">入力</label>
+        <input id="input" type="text" ref={inputValue} />
+      </div>
+      <div>
+        <label htmlFor="show">表示</label>
+        <input id="show" type="text" value={str} />
+      </div>
+      <button onClick={() => {
+        const s = inputValue.current && inputValue.current.value
+        setStrState(s || "")
+      }}>set</button>
     </div>
   );
 }
