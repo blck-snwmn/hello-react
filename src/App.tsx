d4d53582b71f47d1c6431e5301c67cf920d9917a
blck-snwmn <whitesnowmancreator@gmail.com>
2019-07-24 19:09:10 +0900

    clean rename variable name for RefObject
    inputValue -> inputRef

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import './App.css';
 
 const App: React.FC = () => {
   const [str, setStrState] = useState("a");
-  const inputValue = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <div className="App">
       <div>
         <label htmlFor="input">入力</label>
-        <input id="input" type="text" ref={inputValue} />
+        <input id="input" type="text" ref={inputRef} />
       </div>
       <div>
         <label htmlFor="show">表示</label>
         <input id="show" type="text" value={str} />
       </div>
       <button onClick={() => {
-        const s = inputValue.current && inputValue.current.value
+        const s = inputRef.current && inputRef.current.value
         setStrState(s || "")
       }}>set</button>
     </div>
