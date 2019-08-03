fd3961d71bb453fedae4e47a079c8c0f17891789
blck-snwmn <whitesnowmancreator@gmail.com>
2019-08-03 23:44:22 +0900

    Remove webscoket code

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,7 @@ import React, { useReducer } from 'react';
 import SideMenu, { menuReducer, initContent } from './components/SideMenu';
 import './App.css';
 
-const webSocketWrap = () => {
-  const ws = new WebSocket("ws://localhost:18888/websocket/send");
-  // MessageEvent extends Event
-  ws.onmessage = (event: MessageEvent) => {
-    console.log("yes", event)
-  }
-  ws.onerror = (event: Event) => {
-    console.log("error", event)
-  }
-  ws.onopen = (event: Event) => {
-    console.log("open", event)
-  }
-  // CloseEvent extends Event
-  ws.onclose = (event: CloseEvent) => {
-    console.log("close", event)
-  }
-  return ws;
-}
-
 const App: React.FC = () => {
-  // const ws = useRef(new WebSocket("ws://localhost:18888/websocket"));
-  const ws = webSocketWrap()
-
   const [menuState, dispatch] = useReducer(menuReducer, {}, initContent)
 
   return (
